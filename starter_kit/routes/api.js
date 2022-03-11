a909a66cdd47447709970173377969926a98c61b
namuun <nrgnamuunaa@gmail.com>
2022-03-11 15:39:41 +0800

    yu yumbe

--- starter_kit/routes/api.js
+++ starter_kit/routes/api.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const FoodController = require('../controller/FoodController');
-const FoodValidator = require('../middleware/create_update')
+// const FoodValidator = require('../middleware/create_update')
 
 // Requests
 
 router.get('/foods', FoodController.get_foods);
 router.get('/user/:userId', FoodController.find_user);
 
-router.post('/createFood', FoodValidator.createfood() , FoodController.create_food)
+router.post('/createFood', FoodController.create_food)
 router.post('/addUser', FoodController.add_user);
 router.post('/updateFood', FoodController.update_food);
 router.post('/deleteOrder/:orderId', FoodController.delete_order)
